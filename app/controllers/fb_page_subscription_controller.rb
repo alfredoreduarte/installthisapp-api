b20006efcc0850be17fbd57c0079a1d66821e128
Alfredo Re <alfredoreduarte@gmail.com>
2016-09-27 00:59:37 -0400

    Testing top fans realtime subscription on staging

--- app/controllers/fb_page_subscription_controller.rb
+++ app/controllers/fb_page_subscription_controller.rb
@@ -9,66 +9,69 @@ class FbPageSubscriptionController < ApplicationController
 	end
 
 	def subscription
-		hash = params
-		logger.info('===== Top Fans Real time entry received =====')
-		logger.info(params.inspect)
-		entries = hash[:entry]
-		entries.each do |entry|
-			entry[:changes].each do |change|
-				logger.info(change)
-				if change[:field] == "feed"
-					value = change[:value]
-					item = value[:item]
-					if item == "like"
-						verb = value[:verb]
-						page_id = value[:post_id].split("_")[0]
-						post_id = value[:post_id].split("_")[1]
-						user_identifier = value[:sender_id]
-						user_name = value[:sender_name]
-						if verb == "remove"
-							TopFansLike.where(
-								post_id: page_id,
-								user_identifier: user_identifier,
-								page_id: page_id,
-							).delete
-						elsif verb == "add"
-							TopFansLike.create(
-								post_id: post_id,
-								user_identifier: user_identifier,
-								user_name: user_name,
-								page_id: page_id,
-							)
-						end
-					elsif item == "comment"
-						verb = value[:verb]
-						page_id = value[:post_id].split("_")[0]
-						post_id = value[:post_id].split("_")[1]
-						comment_id = value[:comment_id].split("_")[1]
-						user_identifier = value[:sender_id]
-						user_name = value[:sender_name]
-						if verb == "remove"
-							TopFansComment.where(
-								post_id: page_id,
-								user_identifier: user_identifier,
-								page_id: page_id,
-								comment_id: comment_id,
-							).delete
-						elsif verb == "add"
-							TopFansComment.create(
-								post_id: post_id,
-								user_identifier: user_identifier,
-								user_name: user_name,
-								page_id: page_id,
-								comment_id: comment_id,
-							)
+		if params[:"hub.verify_token"]
+			logger.info('===== Top Fans Real time Verification =====')
+			logger.info('verify_token = ')
+			logger.info(params[:"hub.verify_token"])
+			render plain: params[:"hub.challenge"]
+		else
+			hash = params
+			logger.info('===== Top Fans Real time entry received =====')
+			logger.info(params.inspect)
+			entries = hash[:entry]
+			entries.each do |entry|
+				entry[:changes].each do |change|
+					logger.info(change)
+					if change[:field] == "feed"
+						value = change[:value]
+						item = value[:item]
+						if item == "like"
+							verb = value[:verb]
+							page_id = value[:post_id].split("_")[0]
+							post_id = value[:post_id].split("_")[1]
+							user_identifier = value[:sender_id]
+							user_name = value[:sender_name]
+							if verb == "remove"
+								TopFansLike.where(
+									post_id: page_id,
+									user_identifier: user_identifier,
+									page_id: page_id,
+								).delete
+							elsif verb == "add"
+								TopFansLike.create(
+									post_id: post_id,
+									user_identifier: user_identifier,
+									user_name: user_name,
+									page_id: page_id,
+								)
+							end
+						elsif item == "comment"
+							verb = value[:verb]
+							page_id = value[:post_id].split("_")[0]
+							post_id = value[:post_id].split("_")[1]
+							comment_id = value[:comment_id].split("_")[1]
+							user_identifier = value[:sender_id]
+							user_name = value[:sender_name]
+							if verb == "remove"
+								TopFansComment.where(
+									post_id: page_id,
+									user_identifier: user_identifier,
+									page_id: page_id,
+									comment_id: comment_id,
+								).delete
+							elsif verb == "add"
+								TopFansComment.create(
+									post_id: post_id,
+									user_identifier: user_identifier,
+									user_name: user_name,
+									page_id: page_id,
+									comment_id: comment_id,
+								)
+							end
 						end
 					end
 				end
 			end
-		end
-		if params[:"hub.challenge"]
-			render plain: params[:"hub.challenge"]
-		else
 			render plain: 'ok'
 		end
 	end
