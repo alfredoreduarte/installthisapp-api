98045ccb98fbafc0118fc9a0c8451b77760c12db
Alfredo Re <alfredoreduarte@gmail.com>
2016-10-17 17:03:33 -0300

    Top Fans tab installation /realtime subscription working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
 	# resources :fb_users
+	match '/applications/:checksum/:action.json', to: "applications#:action", via: [:get, :post, :delete, :put, :patch]
 
+	# 
+	# CANVAS
+	# 
+	match '/auth.json', to: "canvas#auth", via: [:post]
+	match '/standalone_auth.json', to: "canvas#standalone_auth", via: [:post]
+	match '/:checksum/:action.json', to: "canvas#:action", via: [:get, :post]
+
+	# 
+	# GODVIEW
+	# 
+	match '/jsonmock', to: "admins#jsonmock", via: [:get]
+
+	# 
+	# FB Page realtime subscriptions
+	# 
+	# match '/top_fans_stats', to: "fb_page_subscription#top_fans_stats", via: [:get]
+	# match '/likes_by_page', to: "fb_page_subscription#likes_by_page", via: [:get]
+	match '/subscription', to: "fb_page_subscription#subscription", via: [:get, :post]
 	get 'fb_profiles/fetch_fb_pages.json', to: 'fb_profiles#fetch_fb_pages'
 	resources :fb_users, path: 'users'
 	resources :applications
@@ -23,24 +42,4 @@ Rails.application.routes.draw do
 		# end
 	# end
 	get 'admins/entities', to: 'admins#entities'
-	match '/applications/:checksum/:action.json', to: "applications#:action", via: [:get, :post, :delete, :put, :patch]
-
-	# 
-	# CANVAS
-	# 
-	match '/auth.json', to: "canvas#auth", via: [:post]
-	match '/standalone_auth.json', to: "canvas#standalone_auth", via: [:post]
-	match '/:checksum/:action.json', to: "canvas#:action", via: [:get, :post]
-
-	# 
-	# GODVIEW
-	# 
-	match '/jsonmock', to: "admins#jsonmock", via: [:get]
-
-	# 
-	# FB Page realtime subscriptions
-	# 
-	# match '/top_fans_stats', to: "fb_page_subscription#top_fans_stats", via: [:get]
-	# match '/likes_by_page', to: "fb_page_subscription#likes_by_page", via: [:get]
-	match '/subscription', to: "fb_page_subscription#subscription", via: [:get, :post]
 end
