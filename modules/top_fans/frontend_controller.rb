e2b4149692a9f009adc7fb8ba09253262c854aa4
Alfredo Re <alfredoreduarte@gmail.com>
2016-10-27 21:47:10 -0300

    Adding date limit to top fans frontend query

--- modules/top_fans/frontend_controller.rb
+++ modules/top_fans/frontend_controller.rb
@@ -6,9 +6,10 @@ module FrontendController
 	end
 	def entries
 		# expires_in 5.minutes, public: true
+		limit_date = $application.setting.conf["preferences"]["start_date"] ? $application.setting.conf["preferences"]["start_date"].to_time : Time.now - 1.years
 		identifier = $application.fb_page.identifier
-		results_likes = TopFansLike.likes_by_page(identifier, $application.setting.conf["preferences"]["ignored_user_identifiers"], 10)
-		results_comments = TopFansComment.comments_by_page(identifier, $application.setting.conf["preferences"]["ignored_user_identifiers"], 10)
+		results_likes = TopFansLike.likes_by_page(identifier, $application.setting.conf["preferences"]["ignored_user_identifiers"], 10, limit_date)
+		results_comments = TopFansComment.comments_by_page(identifier, $application.setting.conf["preferences"]["ignored_user_identifiers"], 10, limit_date)
 		response = {
 			status: 'ok',
 			likes: results_likes,
