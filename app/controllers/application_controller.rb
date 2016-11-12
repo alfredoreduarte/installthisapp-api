5598d7bd848bc216381b6c689965ad462ca8bb6c
Alfredo Re <alfredoreduarte@gmail.com>
2016-11-12 00:21:16 -0300

    Commenting out super-admin godview because it breaks fb_auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,12 @@ class ApplicationController < ActionController::Base
 	# Godview for super admins
 	# Swaps the current authenticated super admin with a client account
 	# 
-	alias_method :devise_current_admin, :current_admin
-	def current_admin
-		if request.headers["spy-user"].to_i > 0
-			devise_current_admin = Admin.find(request.headers["spy-user"].to_i)
-		else
-			devise_current_admin = Admin.find_by(uid: request.headers["uid"])
-		end
-	end
+	# alias_method :devise_current_admin, :current_admin
+	# def current_admin
+	# 	if request.headers["spy-user"].to_i > 0
+	# 		devise_current_admin = Admin.find(request.headers["spy-user"].to_i)
+	# 	else
+	# 		devise_current_admin = Admin.find_by(uid: request.headers["uid"])
+	# 	end
+	# end
 end
--- app/controllers/fb_profiles_controller.rb
+++ app/controllers/fb_profiles_controller.rb
@@ -1,20 +1,12 @@
 class FbProfilesController < ApplicationController
 	before_action :authenticate_admin!
 
-	def show
-		
-	end
-
 	def create
 		current_admin.fb_profile = FbProfile.where(identifier: params[:identifier]).first_or_initialize
 		current_admin.fb_profile.signed_request = params[:signed_request]
 		if current_admin.fb_profile.sign_in
 			current_admin.fb_profile.save
 			@admin = current_admin
-			# render json: {
-				# status: 'success',
-				# data: current_admin.fb_profile.as_json
-			# }
 			render 'admins/entities'
 		else
 			render json: {
@@ -26,11 +18,8 @@ class FbProfilesController < ApplicationController
 	def fetch_fb_pages
 		current_admin.fb_profile.fetch_fb_pages
 		response = {
-			# apps: current_admin.applications,
 			pages: current_admin.fb_profile.fb_pages,
 		}
 		render json: response
-		# render json: { status: 'success' }
-		# render json: { pages: current_admin.fb_profile.fb_pages.as_json(only: [:identifier, :name, :like_count]) }
 	end
 end
