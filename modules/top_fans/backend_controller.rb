a38d32c317629d6e2dd22594a43ca67ed711e69a
Alfredo Re <alfredoreduarte@gmail.com>
2016-11-14 13:29:06 -0300

    Processing post list

--- modules/top_fans/backend_controller.rb
+++ modules/top_fans/backend_controller.rb
@@ -3,9 +3,53 @@ module BackendController
 		render json: @application.setting
 	end
 	def entries
+		# test
+		# page = FbGraph2::Page.new(@application.fb_page.identifier).fetch(access_token: @application.admin.fb_profile.access_token, fields: :access_token)
+		# page = FbGraph2::Page.new(@application.fb_page.identifier).fetch(
+			# :access_token => @application.admin.fb_profile.access_token, 
+			# :fields => :access_token
+		# )
+		# fb_since = Date.today - 1.month
+		# feed = page.feed(:since => fb_since, :fields => "actions, message, picture, icon, from, likes.summary(1), comments.summary(1)", :limit => 100)
+		# 
+
+		# Koala
+		user_graph = Koala::Facebook::API.new(@application.admin.fb_profile.access_token)
+		# graph_fb_p = self.graph_facebook_page(fb_page_identifier)
+		page_token = user_graph.get_page_access_token(@application.fb_page.identifier)
+		koala = Koala::Facebook::API.new(page_token)
+		elfeed = koala.get_connection('me', 'feed')
+		# primero = elfeed.second
+		# post = elfeed.next_page.first
+		post = elfeed.third
+		# elfeed.each do |post|
+			# logger.info post["story"]
+			if post["comments"]
+				logger.info "=== Comments ==="
+				post["comments"]["data"].each do |comment|
+					logger.info "#{comment["from"]["name"]}: #{comment["message"]}"
+				end
+			end
+			if post["comments"].next_page
+				logger.info "=== Comments ==="
+				post["comments"].next_page["data"].each do |comment|
+					logger.info "#{comment["from"]["name"]}: #{comment["message"]}"
+				end
+			end
+			if post["likes"]
+				logger.info "=== Likers ==="
+				post["likes"]["data"].each do |like|
+					logger.info like["name"]
+				end
+			end
+		# end
+		# logger.info "traigo"
+		# logger.info primero["comments"]["data"].first["message"]
+		# logger.info primero
+
 		# remove this after dumping top fans in V2
-		los_ids = FbPage.pluck(:identifier)
-		TopFansCleanupJob.perform_later(los_ids)
+		# los_ids = FbPage.pluck(:identifier)
+		# TopFansCleanupJob.perform_later(los_ids)
 		# 
 		if @application.fb_page
 			identifier = @application.fb_page.identifier
