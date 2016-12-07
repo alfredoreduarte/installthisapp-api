e48b4b99b312ae6a70648d77085397a97f63aaf3
Alfredo Re <alfredoreduarte@gmail.com>
2016-12-07 11:05:45 -0300

    Logging fb page errors

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -2,6 +2,6 @@ class MainController < ApplicationController
 	# before_action :authenticate_admin!, except: [:index]
 	# before_action :authenticate_admin!
 	def index
-		render plain: 'authenticated response fuck yeah'
+		render plain: 'Server is up and running'
 	end
 end
