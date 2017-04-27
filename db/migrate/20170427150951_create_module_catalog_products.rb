fe5b91807f1b6db4ee23635c5b7a442016e1883e
Alfredo Re <alfredoreduarte@gmail.com>
2017-04-27 11:43:39 -0400

    Created Product migration

---
+++ db/migrate/20170427150951_create_module_catalog_products.rb
@@ -0,0 +1,36 @@
+class CreateModuleCatalogProducts < ActiveRecord::Migration[5.0]
+	def change
+		create_table :module_catalog_products do |t|
+			# Basic metadata
+			t.string :name
+			t.string :slug
+			t.integer :status, default: 0
+			t.boolean :featured, default: false
+			
+			# Product details
+			t.text :description
+			t.string :short_description
+
+			# Price
+			t.string :price
+			t.string :regular_price
+			t.string :sale_price
+
+			# Sales
+			t.datetime :on_sale_from
+			t.datetime :on_sale_to
+
+			# Order
+			t.integer :menu_order
+
+			# Product categories
+			t.text :category_ids, array: true, default: []
+
+			# Attached media
+			t.integer :featured_image_id
+			t.text :gallery_media_ids, array: true, default: []			
+
+			t.timestamps
+		end
+	end
+end
