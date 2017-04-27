b27bf5ae51ef91d48103894970c7704516743050
Alfredo Re <alfredoreduarte@gmail.com>
2017-04-27 17:39:09 -0400

    Created catalog categories table

---
+++ db/migrate/20170427200407_create_module_catalog_categories.rb
@@ -0,0 +1,18 @@
+class CreateModuleCatalogCategories < ActiveRecord::Migration[5.0]
+	def change
+		create_table :module_catalog_categories do |t|
+			# Mandatory fields for the awesome_nested_set gem
+			t.string :name
+			t.integer :parent_id, null: true, index: true
+			t.integer :lft, null: false, index: true
+			t.integer :rgt, null: false, index: true
+			# Optional fields
+			t.integer :depth, null: false, default: 0
+			t.integer :children_count, null: false, default: 0
+
+			# Custom fields
+			t.integer :featured_image_id
+			t.string :slug
+		end
+	end
+end
