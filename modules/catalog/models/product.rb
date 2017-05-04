1d0be2b5d7538a2bbb63e1c08e0001d95ddb4208
Alfredo Re <alfredoreduarte@gmail.com>
2017-05-04 12:25:36 -0400

    Sanitizing image ids array before save

--- modules/catalog/models/product.rb
+++ modules/catalog/models/product.rb
@@ -3,6 +3,7 @@ class CatalogProduct < ActiveRecord::Base
 	enum status: { draft: 0, published: 1, deleted: 2 }
 	belongs_to 	:application
 	before_save :sanitize_category_ids
+	before_save :sanitize_image_ids
 
 	def categories
 		return CatalogCategory.find(self.category_ids)
@@ -12,5 +13,11 @@ class CatalogProduct < ActiveRecord::Base
 		category_ids = self.category_ids.map(&:to_i)
 		category_ids = category_ids.uniq
 		self.category_ids = category_ids
+	end
+
+	def sanitize_image_ids
+		gallery_media_ids = self.gallery_media_ids.map(&:to_i)
+		gallery_media_ids = gallery_media_ids.uniq
+		self.gallery_media_ids = gallery_media_ids
 	end
 end
