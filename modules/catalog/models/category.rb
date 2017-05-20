4bcc4fda6ee22406451dcc54b516bd199e1d7dd4
Alfredo Re <alfredoreduarte@gmail.com>
2017-05-20 12:32:44 -0400

    Sanitizing categories slug before save

--- modules/catalog/models/category.rb
+++ modules/catalog/models/category.rb
@@ -2,8 +2,14 @@ class CatalogCategory < ActiveRecord::Base
 	self.table_name = "module_catalog_categories"
 	acts_as_nested_set :counter_cache => :children_count
 	belongs_to 	:application
+	before_save :sanitize_slug
 
 	def permalink
 		return "/#{self.application.fb_application.canvas_id}/#{self.application.checksum}/categories/#{self.slug}"
+	end
+
+	def sanitize_slug
+		slug = self.slug ? self.slug : self.name
+		self.slug = slug.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
 	end
 end
