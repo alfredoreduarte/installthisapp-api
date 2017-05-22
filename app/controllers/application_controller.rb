c237550a96e1f2e64cd2e11d1fba9338cd296ef3
Alfredo Re <alfredoreduarte@gmail.com>
2017-05-22 17:48:24 -0400

    Handling admin update params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,27 @@
 class ApplicationController < ActionController::Base
-	# respond_to :json
-	# protect_from_forgery with: :null_session
+	respond_to :json
+	protect_from_forgery with: :null_session
 	include DeviseTokenAuth::Concerns::SetUserByToken
-	# before_action :configure_permitted_parameters, if: :devise_controller?
+	before_action :configure_permitted_parameters, if: :devise_controller?
 
 	# 
 	# Godview for super admins
 	# Swaps the current authenticated super admin with a client account
 	# 
-	alias_method :devise_current_admin, :current_admin
-	def current_admin
-		if request.headers["spy-user"].to_i > 0
-			devise_current_admin = Admin.find(request.headers["spy-user"].to_i)
-		else
-			devise_current_admin = Admin.find_by(uid: request.headers["uid"])
-		end
-	end
+	# alias_method :devise_current_admin, :current_admin
+	# def current_admin
+	# 	if request.headers["spy-user"].to_i > 0
+	# 		devise_current_admin = Admin.find(request.headers["spy-user"].to_i)
+	# 	else
+	# 		devise_current_admin = Admin.find_by(uid: request.headers["uid"])
+	# 	end
+	# end
 
 	def payola_can_modify_subscription?(subscription)
 		subscription.owner == current_admin
 	end
 
-	# protected
+	protected
 
 	# def configure_permitted_parameters
 		# devise_parameter_sanitizer.permit(:sign_up, keys: [:confirm_success_url])
@@ -33,9 +33,12 @@ class ApplicationController < ActionController::Base
 	# 	Admin::ParameterSanitizer.new(Admin, :admin, params)
 	# end
 
-	# def configure_permitted_parameters
-	# 	devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-	# 		user_params.permit({ roles: [] }, :email, :password, :password_confirmation, :confirm_success_url)
-	# 	end
-	# end
+	# 
+	# Allow extra params for signup, update, etc.
+	# https://github.com/plataformatec/devise#strong-parameters
+	# 
+	def configure_permitted_parameters
+		devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+		devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+	end
 end
