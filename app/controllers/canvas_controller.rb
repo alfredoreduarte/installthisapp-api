a273dbd27b3e70e36a6abb834542a73d388fe89f
Alfredo Re <alfredoreduarte@gmail.com>
2017-06-01 19:57:49 -0400

    Validating params to fix #7

--- app/controllers/canvas_controller.rb
+++ app/controllers/canvas_controller.rb
@@ -33,54 +33,61 @@ class CanvasController < ApplicationController
 	end
 
 	def standalone_auth
-		application = Application.find_by(checksum: params[:checksum])
-		image_dict_assets = application.application_assets.where(attachment_file_name: "images.json")
-		response = {
-			title: application.title,
-			checksum: application.checksum,
-			fb_application_id: application.fb_application.app_id,
-			stylesheet_url: application.application_assets.where(attachment_file_name: "styles.css").last.asset_url,
-			messages_url: application.application_assets.where(attachment_file_name: "messages.json").last.asset_url,
-			# images_url: application.application_assets.where(attachment_file_name: "images.json").last.asset_url,
-			images_url: image_dict_assets.length > 0 ? image_dict_assets.last.asset_url : nil,
-		}
-		respond_to do |format|
-			format.json { render json: response }
+		if params[:checksum]
+			application = Application.find_by(checksum: params[:checksum])
+			if application
+				image_dict_assets = application.application_assets.where(attachment_file_name: "images.json")
+				response = {
+					title: application.title,
+					checksum: application.checksum,
+					fb_application_id: application.fb_application.app_id,
+					stylesheet_url: application.application_assets.where(attachment_file_name: "styles.css").last.asset_url,
+					messages_url: application.application_assets.where(attachment_file_name: "messages.json").last.asset_url,
+					images_url: image_dict_assets.length > 0 ? image_dict_assets.last.asset_url : nil,
+				}
+				respond_to do |format|
+					format.json { render json: response }
+				end
+			else
+				raise ActiveRecord::RecordNotFound, "No Application found for checksum #{params[:checksum]}"
+			end
+		else
+			raise ParamsVerificationFailed, 'checksum'
 		end
 	end
 
 	def auth
-		fb_application = FbApplication.find_by(canvas_id: params[:canvas_id])
-		fb_connection = FbGraph2::Auth.new(fb_application.app_id, fb_application.secret_key)
-		fb_auth = fb_connection.from_signed_request(params[:signed_request])
-		page_data = fb_auth.payload[:page]
-		fb_page = FbPage.find_by(identifier: page_data['id'])
-		application = fb_application.applications.installed.where(fb_page_id: fb_page.id).first
-		if application	
-			application.module
-			response = {
-				title: application.title,
-				checksum: application.checksum,
-				fb_application_id: application.fb_application.app_id,
-				stylesheet_url: application.application_assets.where(attachment_file_name: "styles.css").last.asset_url,
-				messages_url: application.application_assets.where(attachment_file_name: "messages.json").last.asset_url,
-				images_url: application.application_assets.where(attachment_file_name: "images.json").last.asset_url,
-			}
-			respond_to do |format|
-				format.json { render json: response }
+		if params[:canvas_id]
+			fb_application = FbApplication.find_by(canvas_id: params[:canvas_id])
+			if fb_application
+				fb_connection = FbGraph2::Auth.new(fb_application.app_id, fb_application.secret_key)
+				fb_auth = fb_connection.from_signed_request(params[:signed_request])
+				page_data = fb_auth.payload[:page]
+				fb_page = FbPage.find_by(identifier: page_data['id'])
+				application = fb_application.applications.installed.where(fb_page_id: fb_page.id).first
+				if application	
+					application.module
+					response = {
+						title: application.title,
+						checksum: application.checksum,
+						fb_application_id: application.fb_application.app_id,
+						stylesheet_url: application.application_assets.where(attachment_file_name: "styles.css").last.asset_url,
+						messages_url: application.application_assets.where(attachment_file_name: "messages.json").last.asset_url,
+						images_url: application.application_assets.where(attachment_file_name: "images.json").last.asset_url,
+					}
+					respond_to do |format|
+						format.json { render json: response }
+					end
+				else
+					render json: {
+						error: "Could not find an application associated to this Facebook Page and Facebook app"
+					}
+				end
+			else
+				raise ActiveRecord::RecordNotFound, "No fb_application found for canvas_id #{params[:canvas_id]}"
 			end
 		else
-			# render plain: "app not found"
-			render json: {
-				error: "Could not find an application associated to this Facebook Page and Facebook app"
-			# }, status: 404
-			}
-			# respond_to do |format|
-			# 	format.json { render json: {
-			# 		status: 'error',
-			# 		message: 'Could not find an application associated to this Facebook Page and Facebook app'
-			# 	}, status: 404 }
-			# end
+			raise ParamsVerificationFailed, 'canvas_id'
 		end
 	end
 
