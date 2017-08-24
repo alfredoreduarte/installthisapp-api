93e3a9ca6db25fd0aac5b51474dfaadc479f2a79
Alfredo Re <alfredoreduarte@gmail.com>
2017-08-24 15:21:50 -0400

    Handling XML post payload type for lead destinations

--- lib/fb_destination_webhook.rb
+++ lib/fb_destination_webhook.rb
@@ -22,7 +22,9 @@ module FbDestinationWebhook
 						req.body = URI.encode(string_to_encode)
 					when 'xml'
 						req.headers['Content-Type'] = 'application/xml; charset=utf-8'
-						# req.body = "#{fb_lead.field_data.to_json}"
+						data = fb_lead.field_data
+						query = data.map{ |datum| "<#{datum["name"]}>#{datum["values"].join}</#{datum["name"]}>"}
+						req.body = query.join
 					when 'raw'
 						# req.body = "#{fb_lead.field_data.to_json}"
 					else
