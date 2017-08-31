623818f43a1efde0e7913eda3010d4cac288e853
Alfredo Re <alfredoreduarte@gmail.com>
2017-08-31 00:06:07 -0400

    Added my email as bcc for lead destination notifications

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -4,6 +4,6 @@ class AdminMailer < ApplicationMailer
 	def fb_lead_destination_email(recipients, admin, fb_lead)
 		@name = admin.first_name || admin.email
 		@content = fb_lead.field_data
-		mail(to: recipients, subject: 'New lead received')
+		mail(to: recipients, bcc: 'alfredoreduarte@gmail.com', subject: 'New lead received')
 	end
 end
