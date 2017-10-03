3631dbbbb544ffa5a0c08c2bbee9226b8793e55f
Alfredo Re <alfredoreduarte@gmail.com>
2017-10-03 11:28:14 -0300

    Added response views for form canvas;Added new ENV vars for resque workers

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
-worker: bundle exec rake resque:pool
+worker: env TERM_CHILD=1 RESQUE_TERM_TIMEOUT=20 bundle exec rake resque:pool
--- modules/form/frontend_controller.rb
+++ modules/form/frontend_controller.rb
@@ -1,8 +1,16 @@
 module FrontendController
 
 	def entities
-		@application = @application
 		@schema = @application.schema
+	end
+
+	def entries_create
+		@entry = @application.entries.new(payload: params[:entry])
+		if @entry.save
+			@success = true
+		else
+			@success = false
+		end
 	end
 	
 end
