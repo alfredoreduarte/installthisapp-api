3d5323744b86be2c48fd4afb545c7b755369670e
Alfredo Re <alfredoreduarte@gmail.com>
2017-11-01 18:30:01 -0300

    Serving cached votes_count field for photo contest

--- modules/capture_the_flag/backend_controller.rb
+++ modules/capture_the_flag/backend_controller.rb
@@ -2,7 +2,7 @@ module BackendController
 
 	def entities
 		@application_log = ApplicationLog.log_by_checksum(@application.checksum)
-		@entries = @application.entries
+		@entries = @application.entries.order("elapsed_seconds DESC")
 	end
 
 end
